cf2cbdf5810f6c45ddfafdf33e62c9a3788f8606
Aman Thapa <amansinghthapa33@gmail.com>
2025-07-20 20:30:31 +0530

    redux and slice

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import appStore from "./utils/appStore";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Body />}>
-          <Route path="login" element={<Login />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={appStore}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,63 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
 
 const Login = () => {
-  return <div>login</div>;
+  const [emailId, setEmailId] = useState("aman@gmail.com");
+  const [password, setPassword] = useState("07aman@$");
+
+  const handleLogin = async () => {
+    try {
+      const user = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log(user);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="flex justify-center">
+      <div className="card card-dash bg-base-300 w-96 mt-6">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+
+          <label className=" mt-2">
+            <span>Email Id</span>
+            <input
+              type="text"
+              placeholder=""
+              className="input input-md"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </label>
+
+          <label className=" mt-4">
+            <span>Password</span>
+            <input
+              type="text"
+              placeholder=""
+              className="input input-md"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+
+          <div className="card-actions justify-center mt-3">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
